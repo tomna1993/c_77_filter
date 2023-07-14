a0840ab97f3a179e31bb79ea0affc012343cc32f
TomasNagy <tomi.nagy@outlook.com>
2023-07-14 10:03:31 +0200

    feat: implement data structures

--- filter.c
+++ filter.c
@@ -4,6 +4,54 @@
 
 #define MAX_CHARS 20
 
+typedef struct BMPFILEHEADER
+{
+    __int16 header_field;
+
+    __int32 BMP_size;
+
+    __int32 reserved;
+
+    __int32 bitmap_data_address;
+}
+BMPFILEHEADER;
+
+typedef struct BMPINFOHEADER
+{
+    __int32 header_size;
+
+    __int32 width;
+
+    __int32 height;
+
+    __int16 color_planes;
+
+    __int16 bits_per_pixel;
+
+    __int32 compression_method;
+
+    __int32 image_size;
+
+    __int32 h_res;
+
+    __int32 v_res;
+
+    __int32 colors_in_palette;
+
+    __int32 important_colors;
+}
+BMPINFOHEADER;
+
+typedef struct PIXEL
+{
+    __int8 blue;
+    __int8 green;
+    __int8 red;
+}
+PIXEL;
+
+
+
 int main(int argc, char **argv)
 {
     if (argc != 3)
@@ -19,6 +67,11 @@ int main(int argc, char **argv)
     strcpy_s (fileOut, MAX_CHARS, argv[2]);
 
     printf("%s - %s\n", fileIn, fileOut);
+
+
+    BMPFILEHEADER bmp_file_header = { 0 };
+    BMPINFOHEADER bmp_info_header = { 0 };
+
 
     FILE *fp = fopen (fileIn, "rb");
 
